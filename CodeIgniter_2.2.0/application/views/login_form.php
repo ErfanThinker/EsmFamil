ac94603cded5e51613ff0c90e97467beca1b2ea5
ErfanThinker <erfan.molaei@gmail.com>
2015-02-21 09:44:48 +0330

    Added login Form

---
+++ CodeIgniter_2.2.0/application/views/login_form.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form>
+نام و نام خانوادگی:<br>
+<br>
+<input type="text" name="name">
+<br><br>
+سال تولد:<br>
+<br>
+<input type="number" name="byear" min="1300" max="1394">
+<br><br>
+ماه تولد:<br>
+<br>
+<input type="number" name="bmonth" min="1" max="12">
+<br><br>
+روز تولد:<br>
+<br>
+<input type="number" name="bday" >
+<br><br>
+ایمیل<br>
+<br>
+<input type="text" name="mail">
+<br><br>
+نام کاربری<br>
+<br>
+<input type="text" name="username">
+<br><br>
+رمز عبور<br>
+<br>
+<input type="password" name="password">
+<br>
+<br>
+عبارت موجود در کادر:<br>
+<input type="text" name="captcha">
+<br>
+<br>
+<input type="submit" value="تایید">
+</form>
+
+
+
+
+
+
+</body>
+</html>
