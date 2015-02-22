de13a5799804ed57fec909f85724036d63e3a2e3
EmadAbdoli <abdoli.mh@gmail.com>
2015-02-22 09:10:56 +0330

    1.login_form changed to farsi format

--- CodeIgniter_2.2.0/application/views/login_form.php
+++ CodeIgniter_2.2.0/application/views/login_form.php
@@ -1,48 +1,122 @@
 <!DOCTYPE html>
-<html >
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<html>
+
+<head>
+
+  <meta charset="UTF-8">
+
+  <title>CodePen - Sign-Up/Login Form</title>
+
+  <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+
+  <link rel="stylesheet" href="css/normalize.css">
+
+    <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+
+</head>
+
 <body>
-<form method="post" action="authentication/registerUser">
-نام و نام خانوادگی:<br>
-<br>
-<input type="text" name="name">
-<br><br>
-سال تولد:<br>
-<br>
-<input type="number" name="byear" min="1300" max="1394">
-<br><br>
-ماه تولد:<br>
-<br>
-<input type="number" name="bmonth" min="1" max="12">
-<br><br>
-روز تولد:<br>
-<br>
-<input type="number" name="bday" >
-<br><br>
-ایمیل<br>
-<br>
-<input type="text" name="email">
-<br><br>
-نام کاربری<br>
-<br>
-<input type="text" name="nickname">
-<br><br>
-رمز عبور<br>
-<br>
-<input type="password" name="password">
-<br>
-<br>
-عبارت موجود در کادر:<br>
-<input type="text" name="captcha">
-<br>
-<br>
-<input type="submit" value="تایید">
-</form>
+
+  <div class="form">
+
+      <ul class="tab-group">
+        <li class="tab active"><a href="#signup">ثبت نام</a></li>
+        <li class="tab"><a href="#login">ورود</a></li>
+      </ul>
+
+      <div class="tab-content">
+        <div id="signup">   
+          <h1>ثبت نام</h1>
+
+          <form action="/" method="post">
+
+          <div class="top-row ">
+            <div class="field-wrap ">
+              <label>
+                نام و نام خانوادگی<span class="req">*</span>
+              </label>
+              <input type="text" required autocomplete="off" />
+            </div>
+
+            <div class="field-wrap">
+              <label>
+                سن(حداقل ۸)<span class="req">*</span>
+              </label>
+              <input type="number" min="8" required autocomplete="off" />
+            </div>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              پست الکترونیکی<span class="req">*</span>
+            </label>
+            <input type="email"required autocomplete="off"/>
+          </div>
+		
+		<div class="field-wrap">
+            <label>
+              نام کاربری<span class="req">*</span>
+            </label>
+            <input type="text"required autocomplete="off"/>
+          </div>
 
 
 
+          <div class="field-wrap">
+            <label>
+              رمز عبور<span class="req">*</span>
+            </label>
+            <input type="password"required autocomplete="off"/>
+          </div>
+          
+	  <div class="field-wrap">
+            <label>
+              تکرار رمز عبور<span class="req">*</span>
+            </label>
+            <input type="repassword"required autocomplete="off"/>
+          </div>
 
+          <button type="submit" class="button button-block"/>ثبت</button>
 
+          </form>
+
+        </div>
+
+        <div id="login">   
+          <h1>وارد شوید</h1>
+
+          <form action="/" method="post">
+
+            <div class="field-wrap">
+            <label>
+              پست الکترونیکی<span class="req">*</span>
+            </label>
+            <input type="email"required autocomplete="off"/>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              رمز ورود<span class="req">*</span>
+            </label>
+            <input type="password"required autocomplete="off"/>
+          </div>
+
+          <p class="forgot"><a href="#">رمز خود را فراموش کرده اید؟</a></p>
+
+          <button class="button button-block"/>ورود</button>
+
+          </form>
+
+        </div>
+
+      </div><!-- tab-content -->
+
+</div> <!-- /form -->
+
+  <script src='js/jquery.js'></script>
+
+  <script src="js/index.js"></script>
 
 </body>
+
 </html>
