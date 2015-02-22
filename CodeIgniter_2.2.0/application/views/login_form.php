dda52093758e68968ee1294e4e32a6d90cc218f7
EmadAbdoli <abdoli.mh@gmail.com>
2015-02-22 10:26:16 +0330

    1.loginForm Added. by Erfan & Sajjad

--- CodeIgniter_2.2.0/application/views/login_form.php
+++ CodeIgniter_2.2.0/application/views/login_form.php
@@ -5,7 +5,7 @@
 
   <meta charset="UTF-8">
 
-  <title>CodePen - Sign-Up/Login Form</title>
+  <title>EsmFamil Registration</title>
 
   <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
 
@@ -28,36 +28,63 @@
         <div id="signup">   
           <h1>ثبت نام</h1>
 
-          <form action="/" method="post">
+          <form action="registerUser" method="post">
 
-          <div class="top-row ">
+          <div class="req">
             <div class="field-wrap ">
               <label>
                 نام و نام خانوادگی<span class="req">*</span>
               </label>
-              <input type="text" required autocomplete="off" />
+              <input type="text" required autocomplete="off" name="name"/>
             </div>
+          </div>
 
-            <div class="field-wrap">
+            <!-- <div class="field-wrap">
               <label>
                 سن(حداقل ۸)<span class="req">*</span>
               </label>
               <input type="number" min="8" required autocomplete="off" />
+            </div> -->
+            <div class="field-wrap">
+              <div class "field-wrap date"> 
+                <label>
+                  سال تولد<span class="req ">*</span>
+                </label>
+                <input type="number" min="1350" required autocomplete="off" name="byear"/>
+              </div>
+              <div class "field-wrap date"> 
+              <label>
+                ماه<span class="req ">*</span>
+              </label>
+              <input type="number" min="1" max="12" required autocomplete="off" name="bmonth"/>
+              </div>
+              <div class "field-wrap date"> 
+              <label>
+                روز<span class="req ">*</span>
+              </label>
+              <input type="number" min="1" max="31" required autocomplete="off" name="bday"/>
+              </div>
             </div>
-          </div>
+            <!-- <div class="field-wrap">
+              
+            </div>
+            <div class="field-wrap">
+              
+            </div> -->
+          
 
           <div class="field-wrap">
             <label>
               پست الکترونیکی<span class="req">*</span>
             </label>
-            <input type="email"required autocomplete="off"/>
+            <input type="email"required autocomplete="off" name="email"/>
           </div>
-		
-		<div class="field-wrap">
+    
+    <div class="field-wrap">
             <label>
               نام کاربری<span class="req">*</span>
             </label>
-            <input type="text"required autocomplete="off"/>
+            <input type="text"required autocomplete="off" name="nickname"/>
           </div>
 
 
@@ -66,14 +93,20 @@
             <label>
               رمز عبور<span class="req">*</span>
             </label>
-            <input type="password"required autocomplete="off"/>
+            <input type="password"required autocomplete="off"  name="password"/>
           </div>
           
-	  <div class="field-wrap">
+        <div class="field-wrap">
             <label>
               تکرار رمز عبور<span class="req">*</span>
             </label>
-            <input type="repassword"required autocomplete="off"/>
+            <input type="password"required autocomplete="off"/>
+          </div>
+          <div class="field-wrap">
+            <label>
+              عبارت موجود در کادر را وارد کنید:<span class="req">*</span>
+            </label>
+            <input type="text"required autocomplete="off" name="captcha"/>
           </div>
 
           <button type="submit" class="button button-block"/>ثبت</button>
@@ -100,6 +133,14 @@
             </label>
             <input type="password"required autocomplete="off"/>
           </div>
+
+
+          <!-- <div class="field-wrap">
+            <label>
+              عبارت موجود در کادر<span class="req">*</span>
+            </label>
+            <input type="text"required autocomplete="off" name="captcha"/>
+          </div> -->
 
           <p class="forgot"><a href="#">رمز خود را فراموش کرده اید؟</a></p>
 
