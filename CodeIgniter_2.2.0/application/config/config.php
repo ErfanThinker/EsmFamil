886eb72b2aea9db02532da547136faff929ca5d4
Erfan <erfan.molaei@gmail.com>
2015-03-06 15:38:30 +0330

    added enc_key

--- CodeIgniter_2.2.0/application/config/config.php
+++ CodeIgniter_2.2.0/application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'fM6SCIBqlThPZhdiT3l7Qx2TDQAyni5A';
 
 /*
 |--------------------------------------------------------------------------
