c79b7122cfb44a867bf4111df6cfe38cf05a72ab
Erfan <erfan.molaei@gmail.com>
2015-03-09 07:46:28 +0330

    merged model

--- CodeIgniter_2.2.0/application/models/gamemodel.php
+++ CodeIgniter_2.2.0/application/models/gamemodel.php
@@ -21,5 +21,75 @@ class Gamemodel extends CI_Model {
         return $query; 
     }
 
+    function checkIfUserIsInGame($nickname){
+        $this->db->select();
+        $this->db->from('esmFamil_game');
+        $this->db->where('creaternickname', $creaternickname);
+        $this->db->where('isfinished', 0);
+        $count = $this->db->count_all_results();
+        if($count == 0){
+            return False;
+        }else{
+            return TRUE; 
+        }
+
+    }
+
+    function addPlayerToGame($gid, $pnickname){
+        $data = array(
+               'gid' => $gid,
+               'pnickname' => $pnickname
+            );
+
+        //Check if game has capacity
+        $this->db->select("maxnumofplayers");
+        $query = $this->db->get_where('esmfamil_game', array('gid' => $gid), 1, 0);
+        $row =  $query->result();
+        $maxNum = $row->maxnumofplayers;
+        
+
+
+        $this->db->select();
+        $this->db->from("esmFamil_game_members");
+        $this->db->where("gid",$gid);
+        $count = $this->db->count_all_results();
+
+
+        if(($maxNum - 1)>$count){
+            $this->db->insert("esmFamil_game_members", $data); 
+            return TRUE;
+        }else {
+            return False;
+        }
+        //TODO:
+    }
+
+
+    function removeGame($gid){
+        $data = array(
+           'gid' => $gid
+        );
+
+        $this->db->delete('esmFamil_game', $data);
+        $this->db->delete('esmFamil_game_members', $data);
+        return $query; 
+    }
+
+    function removePlayerFromGame($gid,$pnickname){
+        $this->db->insert('esmFamil_game_members', array(
+           'gid' => $gid,
+           'pnickname' => $pnickname
+        ));
+        return $query; 
+    }
+
+    function getListOfGames(){
+        return $this->db->get_where('esmFamil_game',array('isfinished' => 0));
+
+    }
+    function getListOfGamesUserCreatedAndFinished($creaternickname){
+        return $this->db->get_where('esmFamil_game',array('isfinished' => 1,'creaternickname' => $creaternickname));
+    }
+
     
 };
