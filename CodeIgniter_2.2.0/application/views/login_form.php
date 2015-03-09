271426230062f4a4fe46e4ee6a912f6cc1a45bfd
hmdgharb <hmd.gharb@gmail.com>
2015-03-09 06:15:08 -0400

    all

--- CodeIgniter_2.2.0/application/views/login_form.php
+++ CodeIgniter_2.2.0/application/views/login_form.php
@@ -7,11 +7,11 @@
 
   <title>EsmFamil Registration</title>
 
-  <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+  <link href="<?php echo asset_url()?>css/css" rel='stylesheet' type='text/css'>
 
-  <link rel="stylesheet" href="../css/css/normalize.css">
+  <link rel="stylesheet" href="<?php echo asset_url()?>css/normalize.css" >
 
-    <link rel="stylesheet" href="../css/css/style.css" media="screen" type="text/css" />
+    <link rel="stylesheet" href="<?php echo asset_url()?>css/style.css" media="screen" type="text/css" />
 
 </head>
 
@@ -31,80 +31,163 @@
 
           <div class="req">
             <div class="field-wrap ">
-              <label style="width: 90%;">
-                نام و نام خانوادگی<span class="req">(اختیاری)</span>
+              <label style="width: 90%;  text-align: center;">
+                نام و نام خانوادگی<span class="req">*</span>
               </label>
-              <input type="text" required autocomplete="on" name="name" style="text-align: right;"/>
+              <input type="text" required autocomplete="on" name="name" style="text-align: center;"/>
             </div>
           </div>
 
-            <div class="field-wrap">
-              <div class "field-wrap date"> 
-                <label style="width: 90%;">
-                  سال تولد<span class="req ">*(بالای ۱۳۵۰)</span>
-                </label style="width: 90%;">
-                <input type="number" min="1350" required autocomplete="off" name="byear"style="text-align: right; direction: rtl;"/>
-              </div>
-            </div>
-    
-            <div class="field-wrap">
-	  <div class "field-wrap date"> 
-              <label style="width: 90%;">
-                ماه<span class="req ">*(بین ۱ الی ۱۲)</span>
-              </label>
-              <input type="number" min="1" max="12" required autocomplete="off" name="bmonth"style="text-align: right; direction: rtl;"/>
-              </div>
-         </div>    
-	<div class="field-wrap">
-              <div class "field-wrap date"> 
-              <label style="width: 90%;">
-                روز<span class="req ">*(بین ۱ الی ۳۱)</span>
-              </label>
-              <input type="number" min="1" max="31" required autocomplete="off" name="bday"style="text-align: right; direction: rtl;"/>
-              </div>
-            </div>
-            <!-- <div class="field-wrap">
+ <div class ="field-wrap date" style="text-align: center; direction: rtl; color: #a0b3b0; font-size: 22px;">  سال
+                <select name="year" id="year" style="color:  #1ab188;">
+                        <option value="1330" >1330</option>
+                        <option value="1331" >1331</option>
+                        <option value="1332" >1332</option>
+                        <option value="1333" >1333</option>
+                        <option value="1334" >1334</option>
+                        <option value="1335" >1335</option>
+                        <option value="1336" >1336</option>
+                        <option value="1337" >1337</option>
+                        <option value="1338" >1338</option>
+                        <option value="1339" >1339</option>
+                        <option value="1340">1340</option>
+                        <option value="1341">1341</option>
+                        <option value="1342">1342</option>
+                        <option value="1343">1343</option>
+                        <option value="1344">1344</option>
+                        <option value="1345">1345</option>
+                        <option value="1346">1346</option>
+                        <option value="1347">1347</option>
+                        <option value="1348">1348</option>
+                        <option value="1349">1349</option>
+                        <option value="1350">1350</option>
+                        <option value="1351">1351</option>
+                        <option value="1352">1352</option>
+                        <option value="1353">1353</option>
+                        <option value="1354">1354</option>
+                        <option value="1355">1355</option>
+                        <option value="1356">1356</option>
+                        <option value="1357">1357</option>
+                        <option value="1358">1358</option>
+                        <option value="1359">1359</option>
+                        <option value="1360">1360</option>
+                        <option value="1361">1361</option>
+                        <option value="1362">1362</option>
+                        <option value="1363">1363</option>
+                        <option value="1364">1364</option>
+                        <option value="1365">1365</option>
+                        <option value="1366">1366</option>
+                        <option value="1367">1367</option>
+                        <option value="1368">1368</option>
+                        <option value="1369">1369</option>
+                        <option value="1370">1370</option>
+                        <option value="1371">1371</option>
+                        <option value="1372">1372</option>
+                        <option value="1373">1373</option>
+                        <option value="1374">1374</option>
+                        <option value="1375">1375</option>
+                        <option value="1376">1376</option>
+                        <option value="1377">1377</option>
+                        <option value="1378">1378</option>
+                        <option value="1379">1379</option>
+                        <option value="1380">1380</option>
+                        <option value="1381">1381</option>
+                        <option value="1382">1382</option>
+                        <option value="1383">1383</option>
+                        <option value="1384">1384</option>
+                        <option value="1385">1385</option>
+                    </select>
+
+         ماه
+                <select name="month" id="month" style="color:  #1ab188;">
+                        <option value="1" >فروردین</option>
+                        <option value="2">اردیبهشت</option>
+                        <option value="3">خرداد</option>
+                        <option value="4">تیر</option>
+                        <option value="5">مرداد</option>
+                        <option value="6">شهریور</option>
+                        <option value="7">مهر</option>
+                        <option value="8">آبان</option>
+                        <option value="9">آذر</option>
+                        <option value="10">دی</option>
+                        <option value="11">بهمن</option>
+                        <option value="12">اسفند</option>
+                    </select>
+
+         روز
+                <select name="day" id="day" style="color:  #1ab188;">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                        <option value="13">13</option>
+                        <option value="14">14</option>
+                        <option value="15">15</option>
+                        <option value="16">16</option>
+                        <option value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                        <option value="20">20</option>
+                        <option value="21">21</option>
+                        <option value="22">22</option>
+                        <option value="23">23</option>
+                        <option value="24">24</option>
+                        <option value="25">25</option>
+                        <option value="26">26</option>
+                        <option value="27">27</option>
+                        <option value="28">28</option>
+                        <option value="29">29</option>
+                        <option value="30">30</option>
+                        <option value="31">31</option>
+        		   
+	         </select>
+                </div>
+ 
               
-            </div>
-            <div class="field-wrap">
-              
-            </div> -->
           
 
           <div class="field-wrap">
-            <label style="width: 90%;">
+            <label style="width: 90%; text-align: center;">
               پست الکترونیکی<span class="req">*(انگلیسی)</span>
             </label>
-            <input type="email"required autocomplete="off" name="email"style="text-align: right;"/>
+            <input type="email"required autocomplete="off" name="email"style="text-align: center;"/>
           </div>
     
     <div class="field-wrap">
-            <label style="width: 90%;">
+            <label style="width: 90%; text-align: center;">
               نام کاربری<span class="req">*</span>
             </label>
-            <input type="text"required autocomplete="off" name="nickname"style="text-align: right;"/>
+            <input type="text"required autocomplete="off" name="nickname"style="text-align: center;"/>
           </div>
 
 
 
           <div class="field-wrap">
-            <label style="width: 90%;">
+            <label style="width: 90%; text-align: center;">
               رمز عبور<span class="req">*</span>
             </label>
-            <input type="password" required autocomplete="off"  name="password"style="text-align: right;"/>
+            <input type="password" required autocomplete="off"  name="password"style="text-align: center;"/>
           </div>
           
         <div class="field-wrap">
-            <label style="width: 90%;">
+            <label style="width: 90%; text-align: center;">
               تکرار رمز عبور<span class="req">*</span>
             </label style="width: 90%;">
-            <input type="password" required autocomplete="off"style="text-align: right;"/>
+            <input type="password" required autocomplete="off"style="text-align: center;"/>
           </div>
           <div class="field-wrap">
-            <label style="width: 90%;">
+            <label style="width: 90%; text-align: center;">
               عبارت موجود در کادر را وارد کنید:<span class="req">*</span>
             </label>
-            <input cid ="<?php $cid?>" type="text"required autocomplete="off" name="captcha"style="text-align: right;"/>
+            <input type="text"required autocomplete="off" name="captcha"style="text-align: center;"/>
           </div>
 
           <button type="submit" class="button button-block"/>ثبت</button>
@@ -119,28 +202,18 @@
           <form action="/" method="post">
 
             <div class="field-wrap">
-            <label style="width: 90%;">
-              پست الکترونیکی<span class="req">*(انگلیسی)</span>
+            <label style="width: 90%; text-align: center;">
+              پست الکترونیکی<span class="req">*</span>
             </label>
-            <input type="email"required autocomplete="off"style="text-align: right;"/>
+            <input type="email"required autocomplete="off"style="text-align: center;"/>
           </div>
 
           <div class="field-wrap">
-            <label style="width: 90%;">
+            <label style="width: 90%; text-align: center;">
               رمز ورود<span class="req">*</span>
             </label>
-            <input type="password"required autocomplete="off"style="text-align: right;"/>
+            <input type="password"required autocomplete="off"style="text-align: center;"/>
           </div>
-
-
-          <!-- <div class="field-wrap">
-            <label style="width: 90%;">
-              عبارت موجود در کادر<span class="req">*</span>
-            </label>
-            <input type="text"required autocomplete="off" name="captcha"style="text-align: right;"/>
-          </div> -->
-
-          <p class="forgot"><a href="#">رمز خود را فراموش کرده اید؟</a></p>
 
           <button class="button button-block"/>ورود</button>
 
@@ -152,9 +225,9 @@
 
 </div> <!-- /form -->
 
-  <script src='../../css/js/index.js'></script>
+  <script src="<?php echo asset_url()?>js/jquery.js"></script>
 
-  <script src="../../css/js/jquery.js"></script>
+  <script src="<?php echo asset_url()?>js/index.js"></script>
 
 </body>
 
