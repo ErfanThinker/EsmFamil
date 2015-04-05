a48fec05a8fb9c3b809640bfb18186590cd7343c
hmdgharb <hmd.gharb@gmail.com>
2015-04-05 08:28:30 -0400

    added.

--- CodeIgniter_2.2.0/application/views/gamelist.php
+++ CodeIgniter_2.2.0/application/views/gamelist.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+
     <meta charset="UTF-8">
     <title >esmFamil</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
